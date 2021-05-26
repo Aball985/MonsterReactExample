1c50b0bf263d878289232c80a270f0b970f3dd66
Aball985 <arball44@gmail.com>
2021-05-26 04:44:51 -0400

    Slight Changes updating

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -1,10 +1,7 @@
 import styles from "./Card.module.scss";
 export default function Card(props) {
   return (
-    <div
-      style={{ backgroundImage: "url(/img/wave.svg)" }}
-      className={styles["card-container"]}
-    >
+    <div className={styles["card-container"]}>
       <img src={`https://robohash.org/${props.monster.id}`} alt="" />
       <h1 className={styles["monster-name"]}>{props.monster.name}</h1>
       <p className={styles["monster-email"]}>{props.monster.email}</p>
